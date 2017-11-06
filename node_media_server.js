23b4c5499573142cba6ab674c0ce50ae31f4f5ae
Mingliang Chen <illuspas@gmail.com>
2017-11-06 16:33:34 +0800

    up

--- node_media_server.js
+++ node_media_server.js
@@ -6,19 +6,25 @@
 
 const NodeRtmpServer = require('./node_rtmp_server');
 const NodeHttpServer = require('./node_http_server');
+const NodeCoreUtils = require('./node_core_utils');
 
 class NodeMediaServer {
-    constructor(config) {  
+    constructor(config) {
         this.sessions = new Map();
         this.publishers = new Map();
         this.idlePlayers = new Set();
         this.nrs = new NodeRtmpServer(config, this.sessions, this.publishers, this.idlePlayers);
         this.nhs = new NodeHttpServer(config, this.sessions, this.publishers, this.idlePlayers);
+        this.nodeEvent = NodeCoreUtils.nodeEvent;
     }
 
     run() {
         this.nrs.run();
         this.nhs.run();
+    }
+
+    on(eventName, listener) {
+        this.nodeEvent.on(eventName, listener);
     }
 
     getSession(id) {
