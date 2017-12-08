c538fd7261bbc6f5166b7d40145312c1a6870411
rebelvg <johnhartigan@ya.ru>
2017-12-08 13:04:17 +0200

    Reformat, auth for channels route.

--- api/routes/channels.js
+++ api/routes/channels.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
 const channelController = require('../controllers/channels');
+const auth = require('../middleware/auth');
 
 let router = express.Router();
 
 module.exports = function (nms) {
-    router.get('/', channelController.getChannels.bind(nms));
-    router.get('/:app/:channel', channelController.getChannel.bind(nms));
+  router.get('/', auth.bind(nms), channelController.getChannels.bind(nms));
+  router.get('/:app/:channel', channelController.getChannel.bind(nms));
 
-    return router;
+  return router;
 };
