f46a0632656588abc99efb61dd13027784ccee99
movilive <movilive@servidor-rtsp-rtmp-redes.c.sonic-base-160815.internal>
2021-02-28 23:34:12 -0300

    standard config file creted

---
+++ app-standard.js
@@ -0,0 +1,85 @@
+const NodeMediaServer = require('./');
+
+const config = {
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60,
+	/*
+    ssl: {
+      port: 443,
+      key: './privatekey.pem',
+      cert: './certificate.pem',
+    }
+	*/
+  },
+  http: {
+    port: 8000,
+    mediaroot: './media',
+    webroot: './www',
+    allow_origin: '*',
+    api: true
+  },
+  https: {
+    port: 8443,
+    key: './privatekey.pem',
+    cert: './certificate.pem',
+  },
+  auth: {
+    api: true,
+    api_user: 'admin',
+    api_pass: 'admin',
+    play: false,
+    publish: false,
+    secret: 'nodemedia2017privatekey'
+  }
+};
+
+
+let nms = new NodeMediaServer(config)
+nms.run();
+
+nms.on('preConnect', (id, args) => {
+  console.log('[NodeEvent on preConnect]', `id=${id} args=${JSON.stringify(args)}`);
+  // let session = nms.getSession(id);
+  // session.reject();
+});
+
+nms.on('postConnect', (id, args) => {
+  console.log('[NodeEvent on postConnect]', `id=${id} args=${JSON.stringify(args)}`);
+});
+
+nms.on('doneConnect', (id, args) => {
+  console.log('[NodeEvent on doneConnect]', `id=${id} args=${JSON.stringify(args)}`);
+});
+
+nms.on('prePublish', (id, StreamPath, args) => {
+  console.log('[NodeEvent on prePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+  // let session = nms.getSession(id);
+  // session.reject();
+});
+
+nms.on('postPublish', (id, StreamPath, args) => {
+  console.log('[NodeEvent on postPublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+});
+
+nms.on('donePublish', (id, StreamPath, args) => {
+  console.log('[NodeEvent on donePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+});
+
+nms.on('prePlay', (id, StreamPath, args) => {
+  console.log('[NodeEvent on prePlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+  // let session = nms.getSession(id);
+  // session.reject();
+});
+
+nms.on('postPlay', (id, StreamPath, args) => {
+  console.log('[NodeEvent on postPlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+});
+
+nms.on('donePlay', (id, StreamPath, args) => {
+  console.log('[NodeEvent on donePlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+});
+
