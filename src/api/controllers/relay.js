c9190189c4fcc8f47c16c61ec60b8a1f6ef87768
Dipak Sarkar <hello@dipaksarkar.in>
2022-07-25 17:57:15 +0530

    Added pai to get relays for a stream

--- src/api/controllers/relay.js
+++ src/api/controllers/relay.js
@@ -3,12 +3,12 @@
 //  illuspas[a]gmail.com
 //  Copyright (c) 2019 Nodemedia. All rights reserved.
 //
-const { get, set } = require('lodash');
+const { get, set } = require("lodash");
 
 function getStreams(req, res, next) {
   let stats = {};
   this.sessions.forEach(function (session, id) {
-    if (session.constructor.name !== 'NodeRelaySession') {
+    if (session.constructor.name !== "NodeRelaySession") {
       return;
     }
 
@@ -16,20 +16,37 @@ function getStreams(req, res, next) {
 
     if (!get(stats, [app, name])) {
       set(stats, [app, name], {
-        relays: []
+        relays: [],
       });
     }
 
-    stats[app][name]['relays'].push({
+    stats[app][name]["relays"].push({
       app: app,
       name: name,
       url: session.conf.ouPath,
       mode: session.conf.mode,
       id: id,
-    })
+    });
   });
 
   res.json(stats);
+}
+
+function getStream(req, res, next) {
+  const relaySession = Array.from(this.sessions.values()).filter(
+    (session) =>
+      session.constructor.name === "NodeRelaySession" &&
+      req.params.app === session.conf.app &&
+      req.params.name === session.conf.name
+  );
+  const relays = relaySession.map((item) => ({
+    app: item.conf.app,
+    name: item.conf.name,
+    url: item.conf.ouPath,
+    mode: item.conf.mode,
+    id: item.id,
+  }));
+  res.json(relays);
 }
 
 function pullStream(req, res, next) {
@@ -50,7 +67,7 @@ function pushStream(req, res, next) {
   let app = req.body.app;
   let name = req.body.name;
   if (url && app && name) {
-    this.nodeEvent.emit('relayPush', url, app, name);
+    this.nodeEvent.emit("relayPush", url, app, name);
     res.sendStatus(200);
   } else {
     res.sendStatus(400);
@@ -61,15 +78,16 @@ function delStream(req, res, next) {
   let relaySession = this.sessions.get(req.params.id);
   if (relaySession) {
     relaySession.end();
-    res.json('Ok');
+    res.json("Ok");
   } else {
-    res.json({ error: 'relay not found' }, 404);
+    res.json({ error: "relay not found" }, 404);
   }
 }
 
 module.exports = {
   getStreams,
+  getStream,
   pullStream,
   pushStream,
-  delStream
+  delStream,
 };
--- src/api/routes/relay.js
+++ src/api/routes/relay.js
@@ -4,6 +4,7 @@ const relayController = require('../controllers/relay');
 module.exports = (context) => {
   let router = express.Router();
   router.get('/', relayController.getStreams.bind(context));
+  router.get('/:app/:name', relayController.getStream.bind(context));
   router.post('/pull', relayController.pullStream.bind(context));
   router.post('/push', relayController.pushStream.bind(context));
   router.delete('/:id', relayController.delStream.bind(context));
