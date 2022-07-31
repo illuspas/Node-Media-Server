7c6aa305c43c6961e608adf1cda1919f9e7cad06
Dipak Sarkar <hello@dipaksarkar.in>
2022-07-31 12:07:29 +0530

    remove log

--- src/node_trans_session.js
+++ src/node_trans_session.js
@@ -65,8 +65,6 @@ class NodeTransSession extends EventEmitter {
     Array.prototype.push.apply(argv, this.conf.acParam);
     Array.prototype.push.apply(argv, ['-f', 'tee', '-map', '0:a?', '-map', '0:v?', mapStr]);
     argv = argv.filter((n) => { return n; }); //去空
-
-    Logger.log(`[Transmuxing Start] args=${JSON.stringify(argv)}`);
     
     this.ffmpeg_exec = spawn(this.conf.ffmpeg, argv);
     this.ffmpeg_exec.on('error', (e) => {
