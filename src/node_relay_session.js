0234576aa255008afbdea4794914587c75833695
Chen Mingliang <illuspas@msn.com>
2022-10-28 21:38:06 +0800

    u

--- src/node_relay_session.js
+++ src/node_relay_session.js
@@ -43,11 +43,11 @@ class NodeRelaySession extends EventEmitter {
     });
 
     this.ffmpeg_exec.stdout.on('data', (data) => {
-      Logger.ffdebug(`FF输出：${data}`);
+      Logger.ffdebug(`FF_LOG:${data}`);
     });
 
     this.ffmpeg_exec.stderr.on('data', (data) => {
-      Logger.ffdebug(`FF输出：${data}`);
+      Logger.ffdebug(`FF_LOG:${data}`);
     });
 
     this.ffmpeg_exec.on('close', (code) => {
