8b3291f6e9f3213fa3eb822d1987cda1bbf49ac5
Linky <127687375+imlinky@users.noreply.github.com>
2023-11-07 17:25:28 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,41 @@
+const NodeMediaServer = require('node-media-server');
+
+const config = {
+  logType: 3,
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 60,
+    ping_timeout: 30
+  },
+  http: {
+    port: 8000,
+    allow_origin: '*'
+  },
+  auth: {
+    play: true,
+    publish: true,
+    secret: 'keydeproduction'
+    api : true,
+    api_user: 'admin',
+    api_pass: 'KanadeXD1',
+  },
+  trans: {
+    ffmpeg: '/usr/local/bin/ffmpeg',
+    tasks: [
+      {
+        app: 'live',
+        hls: true,
+        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+        hlsKeep: true, // to prevent hls file delete after end the stream
+        dash: true,
+        dashFlags: '[f=dash:window_size=3:extra_window_size=5]',
+        dashKeep: true // to prevent dash file delete after end the stream
+      }
+    ]
+  }
+};
+
+var nms = new NodeMediaServer(config)
+nms.run();
