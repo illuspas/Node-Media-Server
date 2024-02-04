94eb04df6e5a80d3b5aaaeb170c4db523a3bb365
Linky <127687375+imlinky@users.noreply.github.com>
2024-02-03 19:13:46 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const NodeMediaServer = require('node-media-server');
 const config = {
   logType: 3,
   rtmp: {
-    port: 1935,
+    port: 80,
     chunk_size: 60000,
     gop_cache: true,
     ping: 60,
     ping_timeout: 30
   },
   http: {
-    port: 8000,
+    port: 80,
     allow_origin: '*'
   },
   auth: {
