4e854db6c70126217615e95f66b0a62f79869a1f
Chen Mingliang <illuspas@msn.com>
2024-12-03 21:22:50 +0800

    Fix bin program https key file path error

--- bin/app.js
+++ bin/app.js
@@ -1,5 +1,17 @@
-const NodeMediaServer = require("..");
+#!/usr/bin/env node
+
+const fs = require("fs");
+const path = require("path");
 const config = require("./config.json");
+const NodeMediaServer = require("..");
+
+if (!fs.existsSync(config.https.key)) {
+  config.https.key = path.join(__dirname, config.https.key);
+
+}
+if (!fs.existsSync(config.https.cert)) {
+  config.https.cert = path.join(__dirname, config.https.cert);
+}
 
 const nms = new NodeMediaServer(config);
 nms.run(); 
