e1b6c4ba82860c6a99598d411cf68bbb8f2d92e4
Chen Mingliang <illuspas@msn.com>
2026-08-22 18:58:53 +0800

    init webadmin

---
+++ webadmin/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
